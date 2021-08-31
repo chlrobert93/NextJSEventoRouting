347e911920cbb8efe7c87ae7f54f8295006fa750
Eder <chlrobert93@gmail.com>
2021-08-31 13:52:07 -0500

    Agregando compoenentes React regulares

---
+++ components/EventList.js
@@ -0,0 +1,11 @@
+import EventItem from "./EventItem";
+
+function EventList(props){
+    const { items } = props;
+
+    return <ul>
+        { items.map(event => <EventItem />) }
+    </ul>
+}
+
+export default EventList;

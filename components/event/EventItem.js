389ba75a260c277019a32dd000d08bc89c2fd62b
Eder <chlrobert93@gmail.com>
2021-08-31 17:06:27 -0500

    Diseño de componentes en proyectos NextJS

--- components/event/EventItem.js
+++ components/event/EventItem.js
@@ -1,32 +1,34 @@
-import Link from 'next/link';
+import Link from "next/link";
+
+import classes from "./event-item.module.css";
 
 function EventItem(props) {
-   const { title, image, date, location, id } = props;
+  const { title, image, date, location, id } = props;
 
-   const humanReadableDate = new Date(date).toLocaleDateString('en-US',{
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric',  
-   });
+  const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
 
-const formattadAddress = location.replace(',', '\n');
-const exploreLink = `/events/${id}`;
+  const formattadAddress = location.replace(",", "\n");
+  const exploreLink = `/events/${id}`;
 
   return (
-    <li>
-      <img src={'/'+image}  alt={title} />
-      <div>
-        <div>
+    <li className={classes.item}>
+      <img src={"/" + image} alt={title} />
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>TITLE</h2>
         </div>
-        <div>
+        <div className={classes.summary}>
           <time>{humanReadableDate}</time>
         </div>
-        <div>
+        <div className={classes.address}>
           <address>{formattadAddress}</address>
         </div>
-        <div>
-            <Link href={exploreLink}>Explore Event</Link>
+        <div className={classes.actions}>
+          <Link href={exploreLink}>Explore Event</Link>
         </div>
       </div>
     </li>

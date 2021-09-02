5d202e709755d9f04a8b2a292b61856795e2ec5d
Eder <chlrobert93@gmail.com>
2021-09-02 12:55:43 -0500

    Navegación a la página eventos filtrados de forma programada

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,14 +1,26 @@
+import { Fragment } from "react";
+import router, { useRouter } from 'next/router';
+
 import { getAllEvents } from "../../dummy-data";
 import EvenList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/event-search";
-import { Fragment } from "react";
+
+
 
 function AllEventsPage() {
+  const router = useRouter();
   const events = getAllEvents();
+
+  function finEventsHandler(year, month){
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+    
+  }
 
   return (
     <Fragment>
-      <EventsSearch />
+      <EventsSearch  onSearch={finEventsHandler} />
       <EvenList  items={events} />
     </Fragment>
   );
